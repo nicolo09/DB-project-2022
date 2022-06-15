6bf482b872209d205c69f185dcbdff754f189f8d
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2022-06-15 19:58:46 +0200

    Added controller

--- app/src/main/java/db/project/controller/Controller.java
+++ app/src/main/java/db/project/controller/Controller.java
@@ -1,5 +1,18 @@
 package db.project.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import db.project.model.Doctor;
+import db.project.model.Patient;
+import db.project.model.Report;
+
 public interface Controller {
+    
+    List<Report> getRefertiByDoctor(Doctor doctor);
+    
+    List<Report> getRefertiByPatient(Patient patient);
+    
+    Optional<Doctor> getDoctorByCF(String CF);
 
 }
--- app/src/main/java/db/project/controller/ControllerImpl.java
+++ app/src/main/java/db/project/controller/ControllerImpl.java
@@ -1,5 +1,30 @@
 package db.project.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import db.project.model.Doctor;
+import db.project.model.Patient;
+import db.project.model.Report;
+
 public class ControllerImpl implements Controller {
+
+    @Override
+    public List<Report> getRefertiByDoctor(Doctor doctor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Report> getRefertiByPatient(Patient patient) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Doctor> getDoctorByCF(String CF) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
