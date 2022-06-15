dbd71d6e50e0d56cad424d5350b62c09c0c6be9b
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2022-06-15 10:01:44 +0200

    Added main View

---
+++ app/src/main/java/DB/project/view/View.java
@@ -0,0 +1,11 @@
+package db.project.view;
+
+public interface View {
+    
+    void goToMainMenu();
+    
+    void goToSearchMenu();
+    
+    void goToModifyMenu();
+
+}
---
+++ app/src/main/java/DB/project/view/ViewImpl.java
@@ -0,0 +1,23 @@
+package db.project.view;
+
+public class ViewImpl implements View {
+
+    @Override
+    public void goToMainMenu() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void goToSearchMenu() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void goToModifyMenu() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
