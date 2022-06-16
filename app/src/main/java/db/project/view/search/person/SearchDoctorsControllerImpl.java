00db73519932423fab87a79510d9eeb7363b48ea
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2022-06-16 09:57:03 +0200

    Removed duplicate search doctor view

--- app/src/main/java/db/project/view/search/person/SearchDoctorsControllerImpl.java
+++ app/src/main/java/db/project/view/search/person/SearchDoctorsControllerImpl.java
@@ -1,11 +1,18 @@
 package db.project.view.search.person;
 
 import db.project.Command;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
-public class SearchDoctorsControllerImpl extends SearchPersonControllerImpl {
+public class SearchMedicsControllerImpl extends SearchPersonControllerImpl {
 
-    public SearchDoctorsControllerImpl(Command onExit) {
+    public SearchMedicsControllerImpl(Command onExit) {
         super(onExit);
     }
 
+    @Override
+    @FXML
+    void onSearch(ActionEvent event) {
+        //TODO: Add medic only filter
+    }
 }
