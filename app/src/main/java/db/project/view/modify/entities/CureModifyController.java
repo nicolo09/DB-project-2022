2fcd4d829d6b0407b501e349153c4c293a02755c
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-06-17 09:48:05 +0200

    Created Controllers for all entities

---
+++ app/src/main/java/db/project/view/modify/entities/CureModifyController.java
@@ -0,0 +1,24 @@
+package db.project.view.modify.entities;
+
+import db.project.view.View;
+import db.project.view.modify.ModifyController;
+
+public class CureModifyController extends ModifyController{
+
+	public CureModifyController(final View view) {
+		super(view);
+	}
+
+	@Override
+	public void addElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ app/src/main/java/db/project/view/modify/entities/PatientModifyController.java
@@ -0,0 +1,24 @@
+package db.project.view.modify.entities;
+
+import db.project.view.View;
+import db.project.view.modify.ModifyController;
+
+public class PatientModifyController extends ModifyController{
+
+	public PatientModifyController(final View view) {
+		super(view);
+	}
+
+	@Override
+	public void addElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
