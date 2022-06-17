d38b0e3ea71120f5fa83a5cd41295bf18f715439
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2022-06-17 16:25:04 +0200

    Implemented ASL entity

---
+++ app/src/main/java/db/project/model/ASL.java
@@ -0,0 +1,15 @@
+package db.project.model;
+
+public interface ASL {
+
+    Integer getCode();
+
+    String getName();
+
+    String getCity();
+
+    String getWay();
+
+    String getNumber();
+
+}
