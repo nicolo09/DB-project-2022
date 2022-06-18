20a2f31fe01b69377f256105828aff1369400f03
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-06-18 10:42:32 +0200

    Created WorkingModifyController

---
+++ app/src/main/java/db/project/view/modify/entities/WorkingModifyController.java
@@ -0,0 +1,24 @@
+package db.project.view.modify.entities;
+
+import db.project.view.View;
+import db.project.view.modify.ModifyController;
+
+public class WorkingModifyController extends ModifyController{
+
+	public WorkingModifyController(View view) {
+		super(view);
+	}
+
+	@Override
+	public void addElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
