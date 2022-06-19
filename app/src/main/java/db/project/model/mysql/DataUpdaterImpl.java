07ac0e83d221f869fdd1ebb5ff331601620196bc
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-06-19 14:45:22 +0200

    Implemented updateAmministratives, updateASL, updateCure, updateEquipment, updateHealtcare, updateHospital, updatePatient, updateUO methods in DataUpdaterImpl

--- app/src/main/java/db/project/model/mysql/DataUpdaterImpl.java
+++ app/src/main/java/db/project/model/mysql/DataUpdaterImpl.java
@@ -3,10 +3,11 @@ package db.project.model.mysql;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Objects;
 import java.util.Optional;
 
+import db.project.model.OPERATION_OUTCOME;
 import db.project.model.TABLES;
 
 public class DataUpdaterImpl implements DataUpdater {
@@ -18,16 +19,16 @@ public class DataUpdaterImpl implements DataUpdater {
 	}
 
 	@Override
-	public boolean updateAmministratives(String CF, Optional<String> role, Optional<Integer> hospitalCode) {
-		if(role.isEmpty() && hospitalCode.isEmpty()) {
-			return false;
+	public OPERATION_OUTCOME updateAmministratives(String CF, Optional<String> role, Optional<Integer> hospitalCode) {
+		if(checkNulls(CF) || checkModifies(role, hospitalCode)) {
+			return OPERATION_OUTCOME.MISSING_ARGUMENTS;
 		}
 		
 		String query = "UPDATE " + TABLES.AMMINISTRATIVE.get() + " SET";
 		query += role.isPresent() ? " Ruolo = '" + role.get() + "'," : "";
 		query += hospitalCode.isPresent() ? " Codice_ospedale = " + hospitalCode.get() + "," : "";
 		query = query.substring(0, query.length() - 1);
-		query += "WHERE Codice_fiscale = ?";
+		query += " WHERE Codice_fiscale LIKE ?";
 		
 		try (final PreparedStatement statement = this.connection.prepareStatement(query)){
 			statement.setString(1, CF);
@@ -35,64 +36,189 @@ public class DataUpdaterImpl implements DataUpdater {
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return false;
+			return OPERATION_OUTCOME.FAILURE;
 		}
 		
-		return true;
+		return OPERATION_OUTCOME.SUCCESS;
 	}
 
 	@Override
-	public boolean updateAppointment(int hospitalCode, int roomNumber, Timestamp date, Optional<Integer> duration,
-			Optional<String> type, Optional<String> patientCF) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean updateASL(int codeASL, Optional<String> name, Optional<String> city, Optional<String> street,
+	public OPERATION_OUTCOME updateASL(int codeASL, Optional<String> name, Optional<String> city, Optional<String> street,
 			Optional<Integer> streetNumber) {
-		// TODO Auto-generated method stub
-		return false;
+		if(checkNulls(codeASL) || checkModifies(name, city, street, streetNumber)) {
+			return OPERATION_OUTCOME.MISSING_ARGUMENTS;
+		}
+		
+		String query = "UPDATE " + TABLES.ASL.get() + " SET";
+		
+		if(city.isPresent() && street.isPresent() && streetNumber.isPresent()) {
+			query += " Ind_Citta = '" + city.get() + "',  Ind_Via = '" + street.get() + "', Ind_Numero_civico = " + streetNumber + ",";
+		} else if(!(city.isEmpty() && street.isEmpty() && streetNumber.isEmpty())) {
+			return OPERATION_OUTCOME.WRONG_INSERTION;
+		}
+		
+		query += name.isPresent() ? " Nome = '" + name.get() + "'," : "";
+		query = query.substring(0, query.length() - 1);
+		query += " WHERE Codice LIKE ?";
+		
+		try (final PreparedStatement statement = this.connection.prepareStatement(query)){
+			statement.setInt(1, codeASL);
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return OPERATION_OUTCOME.FAILURE;
+		}
+		
+		
+		return OPERATION_OUTCOME.SUCCESS;
 	}
 
 	@Override
-	public boolean updateCure(String patientCF, int hospitalCode, String unitName, Optional<Date> ingressDate,
-			Optional<Date> exitDate, Optional<String> description) {
-		// TODO Auto-generated method stub
-		return false;
+	public OPERATION_OUTCOME updateCure(String patientCF, int hospitalCode, String unitName, Optional<Date> exitDate, Optional<String> description) {
+		if(checkNulls(patientCF, hospitalCode, unitName) || checkModifies(exitDate, description)) {
+			return OPERATION_OUTCOME.MISSING_ARGUMENTS;
+		}
+		
+		String query = "UPDATE " + TABLES.CURE.get() + " SET";
+		query += exitDate.isPresent() ? " Data_uscita = " + new java.sql.Date(exitDate.get().getTime()) + "," : "";
+		query += description.isPresent() ? " Motivazione = '" + description.get() + "'," : "";
+		query = query.substring(0, query.length() - 1);
+		query += " WHERE Paziente LIKE ? AND Codice_ospedale LIKE ? AND Nome_unita LIKE ?";
+		
+		try (final PreparedStatement statement = this.connection.prepareStatement(query)){
+			
+			statement.setString(1, patientCF);
+			statement.setInt(2, hospitalCode);
+			statement.setString(3, unitName);
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return OPERATION_OUTCOME.FAILURE;
+		}
+		
+		
+		return OPERATION_OUTCOME.SUCCESS;
 	}
 
 	@Override
-	public boolean updateEquipment(int hospitalCode, int inventoryCode, Optional<String> name,
-			Optional<Date> lastMaintenance) {
-		// TODO Auto-generated method stub
-		return false;
+	public OPERATION_OUTCOME updateEquipment(int hospitalCode, int inventoryCode, Optional<Date> lastMaintenance) {
+		if(checkNulls(hospitalCode, inventoryCode) || checkModifies(lastMaintenance)) {
+			return OPERATION_OUTCOME.MISSING_ARGUMENTS;
+		}
+		
+		String query = "UPDATE " + TABLES.EQUIPMENT.get() + " SET";
+		query += " Data_manutenzione = " + new java.sql.Date(lastMaintenance.get().getTime());
+		query += " WHERE Codice_ospedale LIKE ? AND Codice_inventario LIKE ?";
+		
+		try (final PreparedStatement statement = this.connection.prepareStatement(query)){
+			
+			statement.setInt(1, hospitalCode);
+			statement.setInt(2, inventoryCode);
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return OPERATION_OUTCOME.FAILURE;
+		}
+		
+		return OPERATION_OUTCOME.SUCCESS;
 	}
 
 	@Override
-	public boolean updateHealtcare(String CF, Optional<String> role) {
-		// TODO Auto-generated method stub
-		return false;
+	public OPERATION_OUTCOME updateHealtcare(String CF, Optional<String> role) {
+		if(checkNulls(CF) || checkModifies(role)) {
+			return OPERATION_OUTCOME.MISSING_ARGUMENTS;
+		}
+		String query = "UPDATE " + TABLES.HEALTHCARE.get() + " SET";
+		query += " Ruole = '" + role.get() + "'";
+		query += " WHERE Codice_fiscale LIKE ?";
+		
+		try (final PreparedStatement statement = this.connection.prepareStatement(query)){
+			
+			statement.setString(1, CF);
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return OPERATION_OUTCOME.FAILURE;
+		}
+		
+		return OPERATION_OUTCOME.SUCCESS;
 	}
 
 	@Override
-	public boolean updateHospital(int structureCode, Optional<String> name, Optional<String> city,
-			Optional<String> street, Optional<Integer> streetNumber, Optional<Integer> codeASL) {
-		// TODO Auto-generated method stub
-		return false;
+	public OPERATION_OUTCOME updateHospital(int structureCode, Optional<String> name, Optional<Integer> codeASL) {
+		if(checkNulls(structureCode) || checkModifies(name, codeASL)) {
+			return OPERATION_OUTCOME.MISSING_ARGUMENTS;
+		}
+		String query = "UPDATE " + TABLES.HOSPITAL.get() + " SET";
+		query += name.isPresent() ? " Nome = '" + name.get() + "'," : "";
+		query += codeASL.isPresent() ? " Cod_ASL = " + codeASL.get() + "," : "";
+		query = query.substring(0, query.length() - 1);
+		query += " WHERE Codice_struttura LIKE ?";
+		
+		try (final PreparedStatement statement = this.connection.prepareStatement(query)){
+			
+			statement.setInt(1, structureCode);
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return OPERATION_OUTCOME.FAILURE;
+		}
+		
+		return OPERATION_OUTCOME.SUCCESS;
 	}
 
 	@Override
-	public boolean updatePatient(String CF, Optional<Date> birthDay, Optional<Integer> codASL) {
-		// TODO Auto-generated method stub
-		return false;
+	public OPERATION_OUTCOME updatePatient(String CF, Optional<Integer> codASL) {
+		if(checkNulls(CF) || checkModifies(codASL)) {
+			return OPERATION_OUTCOME.MISSING_ARGUMENTS;
+		}
+		String query = "UPDATE " + TABLES.PATIENT.get() + " SET";
+		query += " Cod_ASL = " + codASL.get();
+		query += " WHERE Codice_fiscale LIKE ?";
+		
+		try (final PreparedStatement statement = this.connection.prepareStatement(query)){
+			
+			statement.setString(1, CF);
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return OPERATION_OUTCOME.FAILURE;
+		}
+		
+		return OPERATION_OUTCOME.SUCCESS;
 	}
 
 	@Override
-	public boolean updateUO(int hospitalCode, String name, Optional<Integer> capacity,
+	public OPERATION_OUTCOME updateUO(int hospitalCode, String name, Optional<Integer> capacity,
 			Optional<Integer> seatsOccupied) {
 		// TODO Auto-generated method stub
+		return OPERATION_OUTCOME.SUCCESS;
+	}
+	
+	private boolean checkNulls(Object ... args) {
+		for (Object object : args) {
+			if(Objects.isNull(object)) {
+				return true;
+			}
+		}
 		return false;
 	}
+	
+	@SafeVarargs
+	private boolean checkModifies(Optional<? extends Object> ... args) {
+		int count = 0;
+		for (Optional<? extends Object> optional : args) {
+			count += optional.isEmpty() ? 1 : 0;
+		}
+		return args.length == count;
+	}
+	
+	
 
 }
