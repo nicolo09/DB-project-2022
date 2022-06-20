afc096103436cf9e45aaeca5e40b7b083f9d2b23
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-06-20 16:25:44 +0200

    Implemented insert, update and remove methods in EquipmentModifyController

--- app/src/main/java/db/project/view/modify/entities/EquipmentModifyController.java
+++ app/src/main/java/db/project/view/modify/entities/EquipmentModifyController.java
@@ -1,34 +1,83 @@
 package db.project.view.modify.entities;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Optional;
+
 import db.project.Command;
 import db.project.controller.Controller;
 import db.project.view.modify.ModifyController;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class EquipmentModifyController extends ModifyController{
 
-	
+	@FXML
+    private DatePicker dpLastMaintenance;
+
+    @FXML
+    private TextField txtCodInventory;
+
+    @FXML
+    private TextField txtCodeHospital;
+
+    @FXML
+    private TextField txtName;
 
 	public EquipmentModifyController(Command exit, Controller mainController) {
 		super(exit, mainController);
 	}
 
 	@Override
+	@FXML
 	public void addElement() {
-		// TODO Auto-generated method stub
-		this.mainController.insertEquipment(0, null, null);
+		var hospitalCode = txtCodeHospital.getText().trim() != "" && isInteger(txtCodeHospital.getText().trim()) ? Integer.parseInt(txtCodeHospital.getText().trim()) : null;
 		
+		var name = txtName.getText().trim() != "" ? txtName.getText().trim() : null;
+		
+		Date lastMaintenance = !Objects.isNull(dpLastMaintenance.getValue()) 
+				? Date.from(Instant.from(dpLastMaintenance.getValue().atStartOfDay(ZoneId.systemDefault()))) 
+				: Date.from(Instant.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault())));
+		
+		this.mainController.insertEquipment(hospitalCode, name, lastMaintenance);
 	}
 
 	@Override
+	@FXML
 	public void updateElement() {
-		// TODO Auto-generated method stub
+		var hospitalCode = txtCodeHospital.getText().trim() != "" && isInteger(txtCodeHospital.getText().trim()) ? Integer.parseInt(txtCodeHospital.getText().trim()) : null;
 		
+		var inventoryCode = txtCodInventory.getText().trim() != "" && isInteger(txtCodInventory.getText().trim()) ? Integer.parseInt(txtCodInventory.getText().trim()) : null;
+		
+		Optional<Date> lastMaintenance = !Objects.isNull(dpLastMaintenance.getValue()) 
+				? Optional.of(Date.from(Instant.from(dpLastMaintenance.getValue().atStartOfDay(ZoneId.systemDefault()))))
+				: Optional.empty();
+
+		this.mainController.updateEquipment(hospitalCode, inventoryCode, lastMaintenance);
 	}
 
 	@Override
+	@FXML
 	protected void removeElement() {
-		// TODO Auto-generated method stub
+		var hospitalCode = txtCodeHospital.getText().trim() != "" && isInteger(txtCodeHospital.getText().trim()) ? Integer.parseInt(txtCodeHospital.getText().trim()) : null;
 		
+		var inventoryCode = txtCodInventory.getText().trim() != "" && isInteger(txtCodInventory.getText().trim()) ? Integer.parseInt(txtCodInventory.getText().trim()) : null;
+		
+		this.mainController.removeEquipment(hospitalCode, inventoryCode);
 	}
+	
+	@FXML
+    void selectEquipment() {
+		//TODO
+    }
+
+    @FXML
+    void selectHospital() {
+    	//TODO
+    }
 
 }
