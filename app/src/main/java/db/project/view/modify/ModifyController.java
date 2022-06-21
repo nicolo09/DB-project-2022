4b6b4ef6cbb7e0213cdeb55c161c789f1852b821
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-06-21 17:46:11 +0200

    Created showOutcome method in abstract class ModifyController

--- app/src/main/java/db/project/view/modify/ModifyController.java
+++ app/src/main/java/db/project/view/modify/ModifyController.java
@@ -2,7 +2,10 @@ package db.project.view.modify;
 
 import db.project.Command;
 import db.project.controller.Controller;
+import db.project.model.OPERATION_OUTCOME;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public abstract class ModifyController {
 	
@@ -35,6 +38,49 @@ public abstract class ModifyController {
 			return true;
 		} catch(NumberFormatException e) {
 			return false;
+		}
+		
+	}
+	
+	protected void showOutcome(final OPERATION_OUTCOME outcome) {
+		
+		Alert alert;
+		
+		switch(outcome) {
+		
+		case CAPACITY_REACHED: alert = new Alert(AlertType.ERROR, "Troppi pazienti in cura, capacità insufficiente");
+			alert.showAndWait();
+			break;
+			
+		case FAILURE: alert = new Alert(AlertType.ERROR, "Qualcosa è andato storto, verificare la connessione o se i dati sono corretti");
+			alert.showAndWait();
+			break;
+			
+		case MISSING_ARGUMENTS: alert = new Alert(AlertType.ERROR, "I dati inseriti sono errati o non sufficienti, riprovare dopo averli compilati");
+			alert.showAndWait();
+			break;
+			
+		case MISSING_PERSON: alert = new Alert(AlertType.ERROR, "Si sta cercando di operare su una persona non registrata" + System.getProperty("line.separator") 
+			+ "verificare la correttezza del codice fiscale o compilare i dati mancanti per registrarla");
+			alert.showAndWait();
+			break;
+			
+		case OVERLAPPING: alert = new Alert(AlertType.ERROR, "È già presente un appuntamento nell'orario richiesto, scegliere un'altra data o orario");
+			alert.showAndWait();
+			break;
+			
+		case SUCCESS: alert = new Alert(AlertType.INFORMATION, "Operazione terminata con successo");
+			alert.showAndWait();
+			break;
+			
+		case WRONG_INSERTION: alert = new Alert(AlertType.ERROR, "I dati inseriti hanno causato un conflitto," + System.getProperty("line.separator")  
+			+ " verificare che siano stati rispettati i vincoli riportati nella finestra di modifica");
+			alert.showAndWait();
+			break;
+			
+		default:
+			break;
+		
 		}
 		
 	}
